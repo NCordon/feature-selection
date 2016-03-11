3ff5de59905808716fc1fbaa33dcbc07de4fc06b
NCordon <nacho.cordon.castillo@gmail.com>
2016-03-11 18:42:42 +0100

    Atributos de data frames normalizados

--- seleccion-caracteristicas.R
+++ seleccion-caracteristicas.R
@@ -1,9 +1,17 @@
 # Limpieza del entorno
 rm(list = ls())
 
+# Paquete para leer los ARFF
 library(foreign)
 
-mov_libras <- read.arff("movement_libras.arff")
+
+# Lectura de datos
+mlibras <- read.arff("movement_libras.arff")
 arrhythmia <- read.arff("arrhythmia.arff")
 wdbc <- read.arff("wdbc.arff")
 
+
+# Normalizamos los nombres de los atributos
+colnames(mlibras) <- tolower(colnames(mlibras))
+colnames(arrhythmia) <- tolower(colnames(arrhythmia))
+colnames(wdbc) <- tolower(colnames(wdbc))
