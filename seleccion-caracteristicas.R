f0260ab365c8b8374360edf6801cffe2fcc552da
NCordon <nacho.cordon.castillo@gmail.com>
2016-03-11 18:24:50 +0100

    Lectura de ficheros hecha

--- seleccion-caracteristicas.R
+++ seleccion-caracteristicas.R
@@ -1,5 +1,9 @@
 # Limpieza del entorno
 rm(list = ls())
 
+library(foreign)
 
+mov_libras <- read.arff("movement_libras.arff")
+arrhythmia <- read.arff("arrhythmia.arff")
+wdbc <- read.arff("wdbc.arff")
 
