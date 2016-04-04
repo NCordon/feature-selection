7011f2b13f03ac334343175017abdb40d1911f5b
NCordon <nacho.cordon.castillo@gmail.com>
2016-04-05 01:26:05 +0200

    Pseudocódigo del SFS añadido

---
+++ pscode/SFS.rb
@@ -0,0 +1,15 @@
+def SFS(){
+    non_selected = {1,...n}
+    mascara = {0,0...0}
+
+    do{
+        j = max arg{tasa(flip(mascara,x)): x$\in$ non_selected}
+
+        if (tasa(flip(mascara,j)) > tasa(mascara)){
+            non_selected = non_selected - j
+            mascara [j] = 1
+        }
+    while mejora
+
+  return mascara
+}
