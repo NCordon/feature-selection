4de7697d3c959c0d2fc4cf38849eb51c1feca0da
NCordon <nacho.cordon.castillo@gmail.com>
2016-04-04 23:30:33 +0200

    Pseudocódigo de la búsqueda tabú añadido

--- pscode/BT.rb
+++ pscode/BT.rb
@@ -1,40 +1,23 @@
 def BT (data){
-    def criterio_asp(mascara){
-        return tasa_acierto(mascara) > tasa_acierto(mejor_mascara)
-    }
+    ind_tabu = 0
 
     while evs_hechas < tope_evs {
         vecinos_generados = sample({1...n}, size=30)
 
         foreach mov in vecinos_generado{
-            if mov is tabu{
-                if criterio_asp(flip(mascara,mov)){
-
+            if mov is not tabu or criterio_asp(flip(mascara,mov)){
+                if tasa_acierto(mascara) > tasa_acierto(mejor_vecino){
+                    mejor_vecino = mascara
+                    tabu = mov
                 }
             }
         }
+        if tasa_acierto(mejor_vecino) > tasa_acierto(mejor_mascara)
+            mejor_mascara = mejor_vecino
 
-      tasa.actual
-    })
-
-    j <- which.max(evs)
-    tasa.mejor.vecino <- evs[j]
-    tabu.elem <- pos.vecinos[j]
-    # Esto asigna a la solución actual el mejor vecino
-    mask[tabu.elem] <- (mask[tabu.elem]+1)%%2
-
-    if (tasa.mejor.vecino > tasa.best){
-      mask.best <- mask
-      tasa.best <- tasa.mejor.vecino
+        tabu_list[ind_tabu] = tabu
+        ind_tabu = ind_tabu % tenencia_tabu
+        evs_hechas = evs_hechas + 30
     }
-
-    # Introducimos en la lista tabú el elemento que ha dado lugar
-    # a la mejor solución del vecindario anterior
-    tabu.list[tl.pos] <- tabu.elem
-    tl.pos <- (tl.pos %% max.tabu) + 1
-
-    # Actualizamos el número de evaluaciones
-    n.eval <- n.eval + length(pos.vecinos)
-  }
     return mejor_mascara
 }
