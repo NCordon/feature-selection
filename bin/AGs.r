e7560f5428432b47a0a457ef76ba4bf862dd6abf
NCordon <nacho.cordon.castillo@gmail.com>
2016-04-22 22:49:02 +0200

    Mutaciones implementadas en el AGG

--- bin/AGs.r
+++ bin/AGs.r
@@ -14,14 +14,16 @@ random.init <- function(data){ sample(0:1, ncol(data)-1, replace=TRUE) }
 ###     generacional
 ###
 ##########################################################################
+population[[c(5,2)]]
+population
 
 make.OX.cruce <- function(mum, dad){
   n <- length(mum)
   cortes <- sample(1:n,2)
-  mum.gen <- seq(cortes[1],cortes[2])
+  mum.cromosomas <- seq(cortes[1],cortes[2])
   
   son <- dad$mask
-  son[mum.gen] <- mum$mask[mum.gen]
+  son[mum.cromosomas] <- mum$mask[mum.cromosomas]
   list(mask = son, fitness=tasa.clas(son))
 }
 
@@ -31,8 +33,10 @@ AGG <- function(data){
   n.eval <- 0
   n.crom <- AGG.n.crom
   prob.cruce <- AGG.prob.cruce
+  prob.mutation <- AGG.prob.mutation
   n.cruces <- ceiling(n.crom*prob.cruce)
-
+  n.mutations <- ceiling(n*prob.mutation)
+  
   population <- lapply(n.crom, function(i){
     mask <- gen.init(data)
     mask.tasa <- tasa.clas(mask)
@@ -44,18 +48,36 @@ AGG <- function(data){
     father <- sample(1:n.crom, n.crom)
     parents <- Map(c,mother,father)     
     
+    # Seleccion
     new.population <- lapply(parents, function(p){
       torneo <- population[p]  
       winner <- which.max(sapply(torneo, function(sol){ sol$fitness }))
       torneo[[winner]]
     })
     
-    
+    # Cruce
     cruces <- lapply(1:n.cruces, function(i){
       make.OX.cruce(new.population[[i]], new.population[[i%%n.cruces + 1]]) 
     })
     
     new.population[1:n.cruces] <- cruces
+    
+    # Mutaciones
+    crom.mutar <- sample(1:n.crom, n.mutations, replace=TRUE)
+    gen.mutar <- sample(1:n, n.mutations, replace=TRUE) 
+    
+    mutations <- lapply(1:n.mutations, function(i){
+      cromosoma <- new.population[[crom.mutar[i]]]
+      gen <- cromosoma$mask[gen.mutar[i]]
+      gen <- (gen+1) %% 2
+      # Mutamos y recalculamos tasa
+      cromosoma$mask[[gen.mutar[i]]] <- gen
+      cromosoma$fitness <- tasa.clas(cromosoma$mask)
+      
+      cromosoma
+    })
+    
+    new.population[crom.mutar] <- mutations
   }
   
   # Devolver algo
