e7981e0f8baf0e6c2d6a1c0738bd3f6432f27603
NCordon <nacho.cordon.castillo@gmail.com>
2016-06-11 04:41:47 +0200

    Añadidas descripciones en pseudocódigo de feromonas y entropía

---
+++ pscode/BLv2.rb
@@ -0,0 +1,19 @@
+def BL(m){
+    # Máscara inicial, por defecto aleatoria
+    mascara = m
+    no_seleccionados = {1,2,...n}
+
+    do{
+        i = random(no_seleccionados)
+        no_seleccionados = no_seleccionados -{i}
+        evs_hechas++
+
+        if (tasa(flip(mascara, i) > mejor_tasa){
+            mascara = flip(mascara,i)
+            mejor_tasa = tasa(mascara)
+            mejora_encontrada = true
+        }
+    }while !no_seleccionados.empty and !mejora_encontrado
+
+  return (mascara, evs_hechas)
+}
